4fe3dd1b35093efc96a0de0b18a3814dfc8e1019
Alif Khaerudin <alifkha7@live.com>
2025-10-16 02:54:06 +0700

    feat: Add EventViewModel to manage UI-related data
    Introduces `EventViewModel` to interact with the `EventRepository` and expose data for the UI.
    
    - The ViewModel takes an `EventRepository` as a dependency.
    - Provides functions to fetch active events (`getEvents`), get event details by ID (`getEventDetail`), and search for events (`searchEvents`).

---
+++ app/src/main/java/dev/alkha/dicodingevent/ui/event/EventViewModel.kt
@@ -0,0 +1,14 @@
+package dev.alkha.dicodingevent.ui.event
+
+import androidx.lifecycle.ViewModel
+import dev.alkha.dicodingevent.data.EventRepository
+
+class EventViewModel(private val eventRepository: EventRepository) : ViewModel() {
+
+    fun getEvents(isActive: Int) = eventRepository.getEvents(isActive)
+
+    fun getEventDetail(id: Int) = eventRepository.getEventDetail(id)
+
+    fun searchEvents(query: String) = eventRepository.searchEvents(query)
+
+}
