6aa0c9a9de3f0ac2ccfafa58e4c32be8a24302da
Alif Khaerudin <alifkha7@live.com>
2025-10-20 02:15:25 +0700

    feat: Add SettingFragment for theme management
    Introduces `SettingFragment`, a new screen that allows users to manage application settings.
    
    This fragment contains a `Switch` for toggling between light and dark themes. It observes theme settings from `SettingViewModel` to update the UI and saves the user's preference when the switch state changes. The theme is applied application-wide using `AppCompatDelegate.setDefaultNightMode`.

---
+++ app/src/main/java/dev/alkha/dicodingevent/ui/setting/SettingFragment.kt
@@ -0,0 +1,52 @@
+package dev.alkha.dicodingevent.ui.setting
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dev.alkha.dicodingevent.databinding.FragmentSettingBinding
+import dev.alkha.dicodingevent.ui.ViewModelFactory
+
+class SettingFragment : Fragment() {
+
+    private var _binding: FragmentSettingBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel by viewModels<SettingViewModel> {
+        ViewModelFactory.getInstance(requireContext())
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentSettingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getThemeSettings().observe(viewLifecycleOwner) { isDarkModeActive: Boolean ->
+            if (isDarkModeActive) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                binding.switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                binding.switchTheme.isChecked = false
+            }
+        }
+
+        binding.switchTheme.setOnCheckedChangeListener { _, isChecked ->
+            viewModel.saveThemeSetting(isChecked)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
