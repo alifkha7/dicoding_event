8e03044f41c5807c1050b359ed9c791627ca3feb
Alif Khaerudin <alifkha7@live.com>
2025-10-19 20:12:32 +0700

    feat: Integrate Room database into repository
    Updates the dependency injection setup to provide the Room database DAO to the `EventRepository`.
    
    - In `Injection.kt`, gets an instance of `EventDatabase` and its `eventDao`.
    - Passes the `eventDao` as a new dependency to the `EventRepository` singleton.

--- app/src/main/java/dev/alkha/dicodingevent/data/Injection.kt
+++ app/src/main/java/dev/alkha/dicodingevent/data/Injection.kt
@@ -1,11 +1,14 @@
 package dev.alkha.dicodingevent.data
 
 import android.content.Context
+import dev.alkha.dicodingevent.data.local.room.EventDatabase
 import dev.alkha.dicodingevent.data.remote.retrofit.ApiConfig
 
 object Injection {
     fun provideRepository(context: Context): EventRepository {
         val apiService = ApiConfig.getApiService()
-        return EventRepository.getInstance(apiService)
+        val database = EventDatabase.getInstance(context)
+        val dao = database.eventDao()
+        return EventRepository.getInstance(apiService, dao)
     }
 }
